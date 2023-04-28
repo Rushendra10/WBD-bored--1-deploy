3197c151e75102004f7a2ed4d801a886f1435d63
Rushendra <rushendra.s20@iiits.in>
2023-04-28 07:26:03 +0530

    solved redis issue

--- routes/feed.js
+++ routes/feed.js
@@ -52,90 +52,90 @@ router.post("/feed", async (req, res) => {
 
     // console.log(req.body.category);
 
-    redisClient.get("posts", async (error, posts) => {
+    // redisClient.get("posts", async (error, posts) => {
 
-        if (error){
-            console.log(error);
-        }
-
-        if (posts!=null){
-            
-            let x = JSON.parse(posts);
-
-            // console.log(x);
-
-            let flag=0;
-
-            for (let i=0; i<x.length; i++){
-
-                if (x[0].postCategory!=req.body.category){
-                    flag=1;
-                    break;
-                }
-            }
-
-            if (flag==0){
-                console.log("Redis Cache Hit");
-
-                res.render("feed1", {posts: x, user: req.session.loggeduser});
-            }
-
-            else {
-                console.log("Redis Cache Miss");   
-                
-                data = await Post.find({$or: [{postCategory: req.body.category}, {newcategoryname: req.body.category}]}, (err, docs) => {
-                    // await Post.find({newcategoryname: "Movies"}, {}, (err, docs) => {
-                    if (docs!=null){
-                        
-                        posts = null;
-                        redisClient.setex("posts", 10, JSON.stringify(docs))
-                        res.render("feed1", {posts: docs, user: req.session.loggeduser});
-                        
-                    }
-                }).clone().catch(function(err){ console.log(err)});
-            }
-            
-        }
-
-        else {
-
-            console.log("Redis Cache Miss");
-
-            data = await Post.find({$or: [{postCategory: req.body.category}, {newcategoryname: req.body.category}]}, (err, docs) => {
-                // await Post.find({newcategoryname: "Movies"}, {}, (err, docs) => {
-                if (docs!=null){
-        
-                    // console.log(docs.length);
-        
-                    // console.log(docs[0]);
-
-                    redisClient.setex("posts", 10, JSON.stringify(docs))
-                    res.render("feed1", {posts: docs, user: req.session.loggeduser});
-                    
-        
-                    
-                }
-            }).clone().catch(function(err){ console.log(err)});
-
-            
-
-            
-
-            // posts = null
-        }
-    })
-
-    // await Post.find({$or: [{postCategory: req.body.category}, {newcategoryname: req.body.category}]}, (err, docs) => {
-    //     // await Post.find({newcategoryname: "Movies"}, {}, (err, docs) => {
-    //     if (docs!=null){
-
-    //         // console.log(docs.length);
-
-    //         // console.log(docs[0]); 
-
-    //         res.render("feed1", {posts: docs , user: req.session.loggeduser});
+    //     if (error){
+    //         console.log(error);
     //     }
-    // }).clone().catch(function(err){ console.log(err)});
+
+    //     if (posts!=null){
+            
+    //         let x = JSON.parse(posts);
+
+    //         // console.log(x);
+
+    //         let flag=0;
+
+    //         for (let i=0; i<x.length; i++){
+
+    //             if (x[0].postCategory!=req.body.category){
+    //                 flag=1;
+    //                 break;
+    //             }
+    //         }
+
+    //         if (flag==0){
+    //             console.log("Redis Cache Hit");
+
+    //             res.render("feed1", {posts: x, user: req.session.loggeduser});
+    //         }
+
+    //         else {
+    //             console.log("Redis Cache Miss");   
+                
+    //             data = await Post.find({$or: [{postCategory: req.body.category}, {newcategoryname: req.body.category}]}, (err, docs) => {
+    //                 // await Post.find({newcategoryname: "Movies"}, {}, (err, docs) => {
+    //                 if (docs!=null){
+                        
+    //                     posts = null;
+    //                     redisClient.setex("posts", 10, JSON.stringify(docs))
+    //                     res.render("feed1", {posts: docs, user: req.session.loggeduser});
+                        
+    //                 }
+    //             }).clone().catch(function(err){ console.log(err)});
+    //         }
+            
+    //     }
+
+    //     else {
+
+    //         console.log("Redis Cache Miss");
+
+    //         data = await Post.find({$or: [{postCategory: req.body.category}, {newcategoryname: req.body.category}]}, (err, docs) => {
+    //             // await Post.find({newcategoryname: "Movies"}, {}, (err, docs) => {
+    //             if (docs!=null){
+        
+    //                 // console.log(docs.length);
+        
+    //                 // console.log(docs[0]);
+
+    //                 redisClient.setex("posts", 10, JSON.stringify(docs))
+    //                 res.render("feed1", {posts: docs, user: req.session.loggeduser});
+                    
+        
+                    
+    //             }
+    //         }).clone().catch(function(err){ console.log(err)});
+
+            
+
+            
+
+    //         // posts = null
+    //     }
+    // })
+
+    await Post.find({$or: [{postCategory: req.body.category}, {newcategoryname: req.body.category}]}, (err, docs) => {
+        // await Post.find({newcategoryname: "Movies"}, {}, (err, docs) => {
+        if (docs!=null){
+
+            // console.log(docs.length);
+
+            // console.log(docs[0]); 
+
+            res.render("feed1", {posts: docs , user: req.session.loggeduser});
+        }
+    }).clone().catch(function(err){ console.log(err)});
     
 
     // console.log("in post");
--- routes/search.js
+++ routes/search.js
@@ -30,9 +30,29 @@ router.post("/search", async (req, res) => {
 
     console.log(req.body.search_string);
 
-    Post.createIndex()
+    // Post.createIndex()
 
-    const posts = await Post.find({});
+    let reqPosts = [];
+
+    const posts = await Post.find({}, (err, docs) => {
+
+        if (docs!=null){
+
+            
+
+            for (let i=0; i<docs.length; i++){
+
+                cont = docs[i].postContent;
+
+                if (cont.search(req.body.search_string)!=-1){
+
+                    reqPosts.push(docs[i]);
+                }
+            }
+
+            // res.render("search", {posts: reqPosts, user: req.session.loggeduser})
+        }
+    }).clone().catch(function(err){ console.log(err)});
 
     // await Post.find({$or: [{postCategory: req.body.category}, {newcategoryname: req.body.category}]}, (err, docs) => {
     //     // await Post.find({newcategoryname: "Movies"}, {}, (err, docs) => {
@@ -48,7 +68,7 @@ router.post("/search", async (req, res) => {
 
     // console.log("in post");
 
-    res.render("search", {posts: [], user: req.session.loggeduser})
+    res.render("search", {posts: reqPosts, user: req.session.loggeduser})
 
 });
 
